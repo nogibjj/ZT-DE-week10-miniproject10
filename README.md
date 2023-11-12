239f486abff7491e5e9462cfdca6743ed8824726
JTuratkhan <108810678+JTuratkhan@users.noreply.github.com>
2023-11-12 04:47:31 +0000

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # ZT-DE-week10-miniproject10
 
 PySpark is the Python API for Apache Spark, an open source, distributed computing framework and set of libraries for real-time, large-scale data processing.
+
+## Project Overview
+This project focuses on leveraging the capabilities of PySpark for processing large-scale datasets. Using PySpark, we perform data transformations and execute Spark SQL queries to extract meaningful insights from a comprehensive dataset of bird species.
+
+## Dataset Description
+The dataset, 'birds.csv', comprises various attributes related to bird species, including 'class id', 'filepaths', 'labels', 'data set', and 'scientific name'. The dataset is substantial in size, making it a fitting choice for demonstrating PySpark's data processing capabilities.
+
+## Data Processing and Analysis
+
+### Transformation
+Data transformation involves filtering the dataset to focus on specific categories and performing group-by operations. Key transformations include:
+
+* Filtering: Records are filtered to isolate specific bird categories, for instance, 'ZEBRA DOVE'.
+* Aggregation: The dataset is aggregated to calculate the average class id for each bird label, offering insights into the distribution of class identifiers across species.
+
+## Spark SQL Query
+A Spark SQL query is implemented to further analyze the dataset:
+
+* The query performs a group-by operation on bird labels to count the number of occurrences of each label, providing a clear view of the dataset's composition regarding bird species.
+
+## Visualizations
+Visualizations created as part of this project provide a graphical representation of our findings, making the data more accessible and understandable. These visualizations are included in the summary report and are also saved as image files within the repository.
--- main.py
+++ main.py
@@ -2,20 +2,69 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('YourAppName').getOrCreate()
 
 # Loading dataset
-file_path = 'dataset.csv'
+file_path = 'birds.csv'
 
 # Load the CSV file into a DataFrame
 df = spark.read.csv(file_path, header=True, inferSchema=True)
 
-# Show the first few rows of the DataFrame
-# df.show()
-
-# Prints the schema of the DataFrame, which gives the data types and column names
-# df.printSchema()
-
-# df.describe().show()
-
+#########################################################################################
 # List all column names
-column_names = df.columns
-print(column_names)
+# column_names = df.columns
+# print(column_names)
 
+# Data Exploration
+# df.show()
+# df.describe().show()
+#########################################################################################
+
+# DATA TRANSFORMATION AND ANALYSIS
+
+# Filtering to get records where the label is "ZEBRA DOVE"
+filtered_df = df.filter(df['labels'] == 'ZEBRA DOVE')
+
+# Display the filtered DataFrame
+# filtered_df.show()
+
+from pyspark.sql import functions as F
+
+# Grouping by 'labels' and calculating the average 'class id'
+aggregated_df = df.groupBy('labels').agg(F.mean('class id').alias('average_class_id'))
+
+# Display the aggregated DataFrame
+aggregated_df.show()
+
+
+# Convert Spark DataFrame to Pandas DataFrame
+pandas_df = aggregated_df.toPandas()
+
+# Create Visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set the aesthetic style of the plots
+sns.set_style("whitegrid")
+
+# Create a bar plot
+plt.figure(figsize=(10, 6))
+sns.barplot(x='labels', y='average_class_id', data=pandas_df)
+
+# Rotate the x labels if they are too cramped
+plt.xticks(rotation=45)
+
+# Add title and labels
+plt.title('Average Class ID for Each Label')
+plt.xlabel('Labels')
+plt.ylabel('Average Class ID')
+
+# Show the plot
+plt.show()
+
+# Save the plot to a file
+plt.savefig('plot.png')
+
+# Register DataFrame as a temporary view
+df.createOrReplaceTempView("birds")
+
+# Spark SQL query
+result = spark.sql("SELECT labels, COUNT(*) as count FROM birds GROUP BY labels")
+result.show()
